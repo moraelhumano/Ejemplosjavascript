ce8699317668eac459c823d2303127a0037d63c7
moraelhumano <morajs@Salvadors-MacBook-Air.local>
2017-10-14 03:17:03 -0500

    se agregaron cosas a los ejecicios

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+
+//Primer ejercicio de la clase para demostra que la sintaxis es identca a los otros lenuajes
+function myFunction() {
+    var greeting;
+    var time = new Date().getHours();
+    if (time < 10) {
+        console.log("Good morning");
+    } else if (time < 20) {
+        console.log("Good day");
+    } else {
+        console.log("Good evening");
+    }
+}
+
+
+//Comenzamos a manejar partes del dom
 function changeRed() {
     document.getElementById('p1').style.color = 'red'
 }
@@ -7,6 +23,9 @@ function changeGreen() {
 }
 
 
+
+
+//Declaramos una variable para remarcar la importancia que tienen los objetos en el lenguaje y definimospropiedades
 var person = {
     firstName: "John",
     lastName: "Doe",
@@ -19,6 +38,8 @@ function objectPerson() {
         person.firstName + " is " + person.age + " years old.";
 }
 
+
+//Definimos metodos
 var person = {
     firstName: "John",
     lastName: "Doe",
@@ -30,12 +51,19 @@ var person = {
 };
 
 function objectPersonMethod() {
-
     document.getElementById("demo").innerHTML = person.fullName();
 }
 
 
-function myFunction() {
+
+
+//Froma viejita de definir una clase
+function PersonaViejita(primerNombre, año) {
+  this.primerNombre = primerNombre;
+  this.diHola = function() {
+  console.log('Hola, Soy ' + this.primerNombre);
+  };
+  this.diTardes = function() {
     var greeting;
     var time = new Date().getHours();
     if (time < 10) {
@@ -45,4 +73,68 @@ function myFunction() {
     } else {
         console.log("Good evening");
     }
+  };
 }
+
+
+
+var persona1 = new PersonaViejita("Alicia");
+var persona2 = new PersonaViejita("Sebastian");
+
+// Llamadas al método diHola de la clase Persona.
+persona1.diHola(); // muestra "Hola, Soy Alicia"
+persona2.diHola(); // muestra "Hola, Soy Sebastian"
+persona2.diTardes(); // muestra "Hola, Soy Sebastian"
+
+//La manera nueva de definir las clases apartir de ecma 2015
+
+class Rectangulo {
+    constructor(base, altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    calcularArea() {
+        return this.base * this.altura;
+    }
+}
+
+var rect = new Rectangulo(10, 5);
+console.log(rect.calcularArea());
+
+
+
+
+
+//La manera nueva de definir las clases apartir de ecma 2015
+  class Persona{
+
+  constructor(primerNombre){
+    this.primerNombre = primerNombre;
+  }
+
+  diHola(){
+  console.log('Hola, Soy ' + this.primerNombre);
+  };
+  diTardes() {
+    var greeting;
+    var time = new Date().getHours();
+    if (time < 10) {
+        console.log("Good morning2");
+    } else if (time < 20) {
+        console.log("Good day2");
+    } else {
+        console.log("Good evening2");
+    }
+  };
+}
+
+
+
+var persona1 = new Persona("AliciaNew");
+var persona2 = new Persona("SebastianNew");
+
+// Llamadas al método diHola de la clase Persona.
+persona1.diHola(); // muestra "Hola, Soy Alicia"
+persona2.diHola(); // muestra "Hola, Soy Sebastian"
+persona2.diTardes(); // muestra "Hola, Soy Sebastian"
